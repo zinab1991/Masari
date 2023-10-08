a661a99bae74cdde047f70cbcb398e8f4180cb1d
Zinab <zbahakim23@twq.idserve.net>
2023-10-08 11:44:58 +0300

    Initial Commit

---
+++ Masari/MasariApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasariApp.swift
+//  Masari
+//
+//  Created by Zinab Zooba on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MasariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
